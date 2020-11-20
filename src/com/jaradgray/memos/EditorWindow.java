b366abe410e51b540c59bac68a871a4e4ef46085
Jarad Gray <jarad.h.gray@gmail.com>
2020-11-20 17:15:52 -0600

    add scrollable text area to editor window

--- src/com/jaradgray/memos/EditorWindow.java
+++ src/com/jaradgray/memos/EditorWindow.java
@@ -1,15 +1,25 @@
 package com.jaradgray.memos;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
+
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class EditorWindow extends JFrame {
 	public EditorWindow(String title) {
 		super(title);
 		
-		// TODO Set layout manager
+		// Set layout manager
+		setLayout(new BorderLayout());
 		
-		// TODO Create Swing components
+		// Create Swing components
+		JTextArea textArea = new JTextArea();
+		JScrollPane scrollPane = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		
-		// TODO Add Swing components to JFrame's content pane
+		// Add Swing components to JFrame's content pane
+		Container c = getContentPane();
+		c.add(scrollPane, BorderLayout.CENTER);
 	}
 }
