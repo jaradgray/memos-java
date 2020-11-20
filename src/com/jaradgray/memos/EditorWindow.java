078912474c22beaf543391ece9c7b8ed2449b62f
Jarad Gray <jarad.h.gray@gmail.com>
2020-11-20 16:49:54 -0600

    create EditorWindow class

---
+++ src/com/jaradgray/memos/EditorWindow.java
@@ -0,0 +1,15 @@
+package com.jaradgray.memos;
+
+import javax.swing.JFrame;
+
+public class EditorWindow extends JFrame {
+	public EditorWindow(String title) {
+		super(title);
+		
+		// TODO Set layout manager
+		
+		// TODO Create Swing components
+		
+		// TODO Add Swing components to JFrame's content pane
+	}
+}
