be4656121f75ea20163b164fc26664806492ac17
Jarad Gray <jarad.h.gray@gmail.com>
2020-11-20 17:14:14 -0600

    set look and feel to that of system

--- src/com/jaradgray/memos/Application.java
+++ src/com/jaradgray/memos/Application.java
@@ -1,9 +1,17 @@
 package com.jaradgray.memos;
 
 import javax.swing.JFrame;
+import javax.swing.UIManager;
 
 public class Application {
 	public static void main(String[] args) {
+		// Make app look like a native Windows app
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
 		// args contains command-line arguments
 		
 		// Create, initialize, and display the editor window
