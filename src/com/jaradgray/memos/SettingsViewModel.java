71c33c2d646d734c49a47aac76c4fb573b608fca
Jarad Gray <jarad.h.gray@gmail.com>
2020-11-22 14:09:45 -0600

    read settings file

--- src/com/jaradgray/memos/SettingsViewModel.java
+++ src/com/jaradgray/memos/SettingsViewModel.java
@@ -3,7 +3,8 @@ package com.jaradgray.memos;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
+
+import org.json.JSONObject;
 
 import com.jaradgray.observable.MutableObservableObject;
 
@@ -15,15 +16,8 @@ public class SettingsViewModel {
 	// Constructor
 	public SettingsViewModel() {
 		// Get settings file data
-		// create path to settings file
-		String settingsFilePath = System.getenv("AppData") + File.separator + "Memos" + File.separator + "settings.txt";
-		System.out.println("settings file path: " + settingsFilePath);
-		File settingsFile = new File(settingsFilePath);
-		if (!settingsFile.exists()) {
-			// TODO create and initialize new settings file
-			System.out.println("TODO create and initialize new settings file");
-		}
-//		String jsonString = getTextFromFile();
+		String jsonString = getTextFromFile(getSettingsFile());
+		System.out.println("jsonString" + jsonString);
 		// Parse data into JSON object
 	}
 	
@@ -33,6 +27,39 @@ public class SettingsViewModel {
 		String result = "";
 		result = System.getProperty("user.home");
 		return result;
+	}
+	
+	private File getSettingsFile() {
+		// Build path string to our settings file
+		// create strings for individual path components
+		final String appDataPath = System.getenv("AppData");
+		final String directoryName = "Memos";
+		final String settingsFileName = "settings.json";
+		// build compound path strings
+		final String memosDirPath = appDataPath + File.separator + directoryName;
+		final String settingsFilePath = memosDirPath + File.separator + settingsFileName;
+		
+		// Create a directory for our app in AppData/Roaming if it doesn't exist
+		File memosDir = new File(memosDirPath);
+		memosDir.mkdirs();
+		
+		// Get settings file, creating it if it doesn't exist
+		File settingsFile = new File(settingsFilePath);
+		// TODO create fresh settings file if there's an error parsing its JSON data
+		if (!settingsFile.exists()) {
+			// TODO create and initialize new settings file
+			// create JSON data the settings file will contain
+			String jsonString = new JSONObject()
+					.put("window_x", 0)
+					.put("window_y", 0)
+					.put("window_width", 600)
+					.put("window_height", 400)
+					.toString();
+			System.out.println("jsonString: " + jsonString);
+			// TODO write jsonString to settings file
+		}
+		
+		return settingsFile;
 	}
 	
 	private String getTextFromFile(File file) {
