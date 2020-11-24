50b6ddf30a0c122446cb7be5743505581a0238bb
Jarad Gray <jarad.h.gray@gmail.com>
2020-11-23 18:29:06 -0600

    implement getBundledThemesJSONObject method

--- src/com/jaradgray/memos/ThemeSettings.java
+++ src/com/jaradgray/memos/ThemeSettings.java
@@ -1,11 +1,22 @@
 package com.jaradgray.memos;
 
 import java.awt.Color;
+import java.util.ArrayList;
 
 import org.json.JSONObject;
 
 public class ThemeSettings {
 	// Constants
+	// bundled themes
+	public static final String BUNDLED_THEME_NAME_0 = "Memos - Light";
+	public static final String BUNDLED_THEME_FG_COLOR_0 = "#000000";
+	public static final String BUNDLED_THEME_BG_COLOR_0 = "#FFFFFF";
+	
+	public static final String BUNDLED_THEME_NAME_1 = "Memos - Dark";
+	public static final String BUNDLED_THEME_FG_COLOR_1 = "#FFFFFF";
+	public static final String BUNDLED_THEME_BG_COLOR_1 = "#000000";
+	
+	// settings keys
 	public static final String KEY_FG_COLOR_TRANSIENT = "fg_color_transient";
 	public static final String KEY_BG_COLOR_TRANSIENT = "bg_color_transient";
 	public static final String KEY_CURRENT_THEME_NAME = "cur_theme_name";
@@ -14,6 +25,9 @@ public class ThemeSettings {
 	public static final String KEY_THEME_NAME = "theme_name";
 	public static final String KEY_FG_COLOR = "fg_color";
 	public static final String KEY_BG_COLOR = "bg_color";
+	
+	public static final String DEFAULT_THEME_NAME = BUNDLED_THEME_NAME_0;
+	
 	
 	// Instance variables
 	private Color mFgColorTransient;	// current non-persisted fg color
@@ -41,5 +55,39 @@ public class ThemeSettings {
 		
 		// TODO create fresh settings file or construct from defaults if there's an
 		//	error parsing the json, or if any key we're interested in doesn't exist
+	}
+	
+	/**
+	 * Constructs a new ThemeSettings with default values
+	 */
+	public ThemeSettings() {
+//		mTheme = new Theme();
+//		mFgColorTransient = new Color(mTheme.getFgColor());
+	}
+	
+	
+	// Public methods
+	/**
+	 * Returns a JSONObject containing one key-value pair, whose key is
+	 * {@link #KEY_ALL_THEMES} and whose value is an array of JSONObjects
+	 * representing all Themes bundled with the app.
+	 * @return
+	 */
+	public static JSONObject getBundledThemesJSONObject() {
+		// Build the Collection containing JSONObjects representing all bundled Themes
+		ArrayList<JSONObject> themesList = new ArrayList<>();
+		JSONObject obj = new JSONObject()
+				.put(KEY_THEME_NAME, BUNDLED_THEME_NAME_0)
+				.put(KEY_FG_COLOR, BUNDLED_THEME_FG_COLOR_0)
+				.put(KEY_BG_COLOR, BUNDLED_THEME_BG_COLOR_0);
+		themesList.add(obj);
+		obj = new JSONObject()
+				.put(KEY_THEME_NAME, BUNDLED_THEME_NAME_1)
+				.put(KEY_FG_COLOR, BUNDLED_THEME_FG_COLOR_1)
+				.put(KEY_BG_COLOR, BUNDLED_THEME_BG_COLOR_1);
+		themesList.add(obj);
+		// Return a JSONObject containing all bundled Theme data
+		return new JSONObject()
+				.put(KEY_ALL_THEMES, themesList);
 	}
 }
