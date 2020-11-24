0bee1828cde034c6f7eb05ed563949328c86192f
Jarad Gray <jarad.h.gray@gmail.com>
2020-11-24 13:59:20 -0600

    create basic ColorChooserPanel class

---
+++ src/com/jaradgray/memos/ColorChooserPanel.java
@@ -0,0 +1,34 @@
+package com.jaradgray.memos;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JColorChooser;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+public class ColorChooserPanel extends JPanel {
+	// Constructor
+	public ColorChooserPanel(Color initialColor, String previewText) {
+		super(new BorderLayout());
+		
+		// Build the GUI
+		
+		// Set up label to use as custom preview panel
+		JLabel previewLabel = new JLabel(previewText);
+		previewLabel.setForeground(Color.green);
+		previewLabel.setBackground(initialColor);
+		previewLabel.setOpaque(true); // is this necessary?
+		previewLabel.setFont(new Font("SansSerif", Font.BOLD, 24));
+		previewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		// Set up ColorChooser
+		JColorChooser colorChooser = new JColorChooser(initialColor);
+		
+		// Add components
+		this.add(previewLabel, BorderLayout.CENTER);
+		this.add(colorChooser, BorderLayout.PAGE_END);
+	}
+}
